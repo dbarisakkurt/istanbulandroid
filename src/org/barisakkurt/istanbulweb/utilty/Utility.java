d5a4707305d363b6633c9f933a8778a012a60c1d
Baris Akkurt <dbarisakkurt@gmail.com>
2013-12-21 13:20:49 +0200

    utility methods added

--- src/org/barisakkurt/istanbulweb/utilty/Utility.java
+++ src/org/barisakkurt/istanbulweb/utilty/Utility.java
@@ -1,15 +1,31 @@
 package org.barisakkurt.istanbulweb.utilty;
 
-import android.app.Activity;
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Utility extends Activity{
-	
-	private Utility() {} //private constructor
-	
-	
-	
+import android.app.Activity;
+
+public class Utility extends Activity {
+
+	private Utility() {
+	} // private constructor
+
+	// is text has min 6 max 18 chars
+	public static boolean isTextInRange(String text) {
+		if (text.length() < 6)
+			return false;
+		else if (text.length() > 18)
+			return false;
+		return true;
+	}
+
+	public static boolean validate(final String hex) {
+		final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+				+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+		Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+		Matcher matcher = pattern.matcher(hex);
+		return matcher.matches();
+	}
 
 }
